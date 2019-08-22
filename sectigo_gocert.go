af593d7087e27ad680524040e315a46cfb4f9698
Aditya <aditya.bhangle@com>
2019-08-22 13:16:11 +0000

    Building scert

--- sectigo_gocert.go
+++ sectigo_gocert.go
@@ -130,12 +130,10 @@ func EnrollCert(d *schema.ResourceData,csrVal string, customerArr map[string]str
 	var sslId = 0
 	var renewId = ""
 	url := d.Get("sectigo_ca_base_url").(string)+"enroll"
-	log.Println("{\"orgId\":"+d.Get("sectigo_cm_orgid").(string)+",\"csr\":\""+csrVal+"\",\"certType\":"+d.Get("cert_type").(string)+",\"numberServers\":"+d.Get("cert_num_servers").(string)+",\"serverType\":"+d.Get("server_type").(string)+",\"term\":"+d.Get("cert_validity").(string)+",\"comments\":\""+d.Get("cert_comments").(string)+"\",\"externalRequester\":\""+d.Get("cert_ext_requester").(string)+"\",\"subjAltNames\":\""+d.Get("subject_alt_names").(string)+"\"}")
 
+	log.Println("{\"orgId\":"+strconv.Itoa(d.Get("sectigo_cm_orgid").(int))+",\"csr\":\""+csrVal+"\",\"certType\":"+strconv.Itoa(d.Get("cert_type").(int))+",\"numberServers\":"+strconv.Itoa(d.Get("cert_num_servers").(int))+",\"serverType\":"+strconv.Itoa(d.Get("server_type").(int))+",\"term\":"+strconv.Itoa(d.Get("cert_validity").(int))+",\"comments\":\""+d.Get("cert_comments").(string)+"\",\"externalRequester\":\""+d.Get("cert_ext_requester").(string)+"\",\"subjAltNames\":\""+d.Get("subject_alt_names").(string)+"\"}")
 	os.Exit(1)
-	var jsonStr = []byte("{\"orgId\":"+d.Get("sectigo_cm_orgid").(string)+",\"csr\":\""+csrVal+"\",\"certType\":"+d.Get("cert_type").(string)+",\"numberServers\":"+d.Get("cert_num_servers").(string)+",\"serverType\":"+d.Get("server_type").(string)+",\"term\":"+d.Get("cert_validity").(string)+",\"comments\":\""+d.Get("cert_comments").(string)+"\",\"externalRequester\":\""+d.Get("cert_ext_requester").(string)+"\",\"subjAltNames\":\""+d.Get("subject_alt_names").(string)+"\"}")
-
-
+	var jsonStr = []byte("{\"orgId\":"+strconv.Itoa(d.Get("sectigo_cm_orgid").(int))+",\"csr\":\""+csrVal+"\",\"certType\":"+strconv.Itoa(d.Get("cert_type").(int))+",\"numberServers\":"+strconv.Itoa(d.Get("cert_num_servers").(int))+",\"serverType\":"+strconv.Itoa(d.Get("server_type").(int))+",\"term\":"+strconv.Itoa(d.Get("cert_validity").(int))+",\"comments\":\""+d.Get("cert_comments").(string)+"\",\"externalRequester\":\""+d.Get("cert_ext_requester").(string)+"\",\"subjAltNames\":\""+d.Get("subject_alt_names").(string)+"\"}")
 	
 	log.Println("Enrolling CERT for "+domain)
 	WriteLogs(d,"Enrolling CERT for "+domain)
